e8889d4f8c324b04c4ba122bd68a2baedb647d9e
Melodi Peace <133246332+PeaceAyomide@users.noreply.github.com>
2024-05-01 22:29:06 +0100

    Update Gallery.jsx

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Gallery = () => {
   return (
-      <div className='gallery-walls  grid max-w-1200px  mx-auto gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))] pt-6rem pl-8 pr-8 pb-12'>
+      <div className='gallery-walls  grid max-w-1200px  mx-auto gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))] pt-6rem pl-8 pr-9 pb-12'>
           <div className='flex justify-center items-center h-400px overflow-hidden'><img src="https://images.unsplash.com/photo-1613366211183-a729adcfc224?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
           <div className='flex justify-center items-center h-400px overflow-hidden'><img src="https://images.unsplash.com/photo-1618721405821-80ebc4b63d26?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
           <div className='flex justify-center items-center h-400px overflow-hidden'><img src="https://images.unsplash.com/photo-1630255733038-0eaa251036c9?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" /></div>
